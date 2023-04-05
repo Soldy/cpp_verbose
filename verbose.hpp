8921bba8cbcbb558c675fe179c581339fe6eb10a
Soldy <4786022+Soldy@users.noreply.github.com>
2023-04-05 20:19:51 +0100

    follow the test
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- verbose.hpp
+++ verbose.hpp
@@ -1,10 +1,11 @@
 #ifndef __CPP_VERBOSE_HPP_
 #define __CPP_VERBOSE_HPP_
 #include <string>
+#include <iostream>
 
 namespace cpp_verbose{
     void msg(std::string msg, std::string element, int level){
-        if(cppConfig::getInt("verbose") >= level)
+        if(cppConfig::getInt("verbose", 0) >= level)
             std::cout << msg << " : " << element << std::endl;
     };
     void msg(
@@ -13,13 +14,23 @@ namespace cpp_verbose{
       std::string error,
       int level
     ){
-        if(cppConfig::getInt("verbose") >  level)
+        if(cppConfig::getInt("verbose", 0) >=  level)
             std::cout <<
                 msg << " : " <<
                 element <<
                 " - " <<
                 error <<
                 std::endl;
+    };
+    void msg(std::string msg, std::string element){
+        cpp_verbose::msg(msg,element,1);
+    };
+    void msg(
+      std::string msg,
+      std::string element,
+      std::string error
+    ){
+        cpp_verbose::msg(msg,element,error,1);
     };
 };
 #endif
